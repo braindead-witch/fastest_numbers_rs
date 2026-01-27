497802f9084f94fe734924595e327f3a2de1e92e
braindead-witch <alt8203719@gmail.com>
2026-01-27 01:35:32 +0100

    Added Dutch language pack

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use fastest_numbers::{
     language::{
         Dictionary,
-        counter::{EnglishRules, LanguageRuleset},
+        counter::{DutchRules, LanguageRuleset},
     },
     math::{export::ExportType, optimize::optimize, stats::Statistics},
 };
 
 fn main() {
-    let dictionary = Dictionary::from_file("languages/en-gb.json");
-    let ruleset = LanguageRuleset::English(EnglishRules);
+    let dictionary = Dictionary::from_file("languages/nl-nl.json");
+    let ruleset = LanguageRuleset::Dutch(DutchRules);
     let optimized = optimize(100000, &dictionary, &ruleset);
-    let _ = optimized.export(ExportType::Json, "results/results_en-gb.json");
+    let _ = optimized.export(ExportType::Json, "results/results_nl-nl.json");
 
     let stats = Statistics::from_optimization_result(optimized);
     println!("Statistics: {}", stats);
